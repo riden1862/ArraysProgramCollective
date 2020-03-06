58f97cb4b119e967d5e05c392df327d955b2e1f1
riden1862 <riden1862@7HGM6Y1.Student.UGDSB.ED>
2020-03-06 15:17:22 -0500

    First commit, math test adventure

---
+++ src/mathtestadventure/MathTestAdventure.java
@@ -0,0 +1,36 @@
+/*
+ Riley Denoon
+MathTestAdventure
+March 6th 2020
+ */
+
+package mathtestadventure;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author riden1862
+ */
+public class MathTestAdventure {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int yes;
+        int no;
+        
+         Scanner keyedInput = new Scanner(System.in);
+        
+        System.out.println("You are a highschool student studing for a math test! It is in two days.");
+        System.out.println("Would you like to study for your test in two days?");
+        
+        if (yes){
+            System.out.println("You are studying");
+        }
+        
+        
+    }
+    
+}
